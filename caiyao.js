b2b15ee10a8ab8918ac7f16cad7482104e05f035
Xiaoshu Lin <xiaoshu.lin@ucloud.cn>
2021-12-03 19:07:59 +0800

    2021-12-03 19:07:57

--- caiyao.js
+++ caiyao.js
@@ -1,26 +1,37 @@
+const m = new Map;
 function output(data, t) {
-  // const m = new Map;
-  let max = 0;
-  function dfs(node, left, gains, rests) {
-    if (left - node[0] < 0) {
-      return;
+  function dfs(node, left, rests) {
+    const key = [...node, left, rests.length];
+    if (m.has(key)) return m.get(key);
+    if (left - node[0] < 0 || rests.length === 0) {
+      m.set(key, 0);
+      m.set(key.join(','), 0);
+      return 0;
     }
-    max = Math.max(max, node[1] + gains);
-    for (let i = 0; i < rests.length; i++) {
-      dfs(rests[i], left - node[0], node[1] + gains, [...rests.slice(0, i), ...rests.slice(i + 1)])
-    }
+    const a = Math.max(
+      ...rests.map(
+        (_, i) => dfs(rests[i], left - node[0], [...rests.slice(0, i), ...rests.slice(i + 1)])
+      )
+    )
+    if (m.has(key.join(',')) && m.get(key.join(',')) !== a + node[1]) console.log(key, a + node[1], m.get(key.join(',')));
+    m.set(key, a + node[1]);
+    m.set(key.join(','), a + node[1]);
+    return a + node[1];
   }
 
-  data.forEach((node, i) => dfs(node, t, 0, [...data.slice(0, i), ...data.slice(i + 1)]));
-  return max;
+  return Math.max(...data.map(
+    (node, i) => dfs(node, t, [...data.slice(0, i), ...data.slice(i + 1)])
+  ))
 }
 
 const r = output(
-  [
-    [71, 100],
-    [69, 1],
-    [1, 2],
-  ],
-  70
+  // [
+  //   [71, 100],
+  //   [69, 1],
+  //   [1, 2],
+  // ],
+  // 70
+  [[71, 26], [34, 59], [82, 30], [23, 19], [1, 66], [88, 85], [12, 94]], 1000
 );
 console.log(r);
+console.log(m);
--- caiyao.py
+++ caiyao.py
@@ -4,23 +4,25 @@ def output (data, t):
     key = "[%d,%d],%d,%d"%(node[0],node[1],left,len(rests))
     if (dp.has_key(key)):
       return dp[key]
-    __max = 0
-    if (left - node[0] < 0):
+    if (left - node[0] < 0 or len(rests) == 0):
+      dp[key] = 0
       return 0
+    __max = 0
     for i, b in enumerate(rests):
       __max = max(__max,dfs(b, left - node[0], rests[0:i]+rests[i+1:]))
     dp[key] = __max+node[1]
-    return __max+node[1]
+    return dp[key]
 
   __max = 0
   for i, node in enumerate(data):
     __max = max(__max,dfs(node, t, data[0:i]+data[i+1:]))
   
-  print(dp)
+  print(dp, len(dp))
   return __max
 
 t, n = map(lambda x:int(x), raw_input().split())
 x = []
 for i in range(0,n):
   x.append(map(lambda x:int(x), raw_input().split()))
+# print(x)
 print(output(x,t))
